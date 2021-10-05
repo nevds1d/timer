dba5580c18a864bb66aee7c02b2571b745deb79d
nevdsid <scottsdale787@gmail.com>
2021-10-05 23:20:39 +0500

    Add methods for buttons

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStartTimer(View view) {
+        isRunning = true;
+    }
+
+    public void onClickPauseTimer(View view) {
+        isRunning = false;
+    }
+
+    public void onClickResetTimer(View view) {
+        isRunning = false;
+        seconds = 0;
     }
 }
